cb903e21712cc61105ed4b97efb2406156d44b9a
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-17 16:15:41 +0200

    feat: Implement #substrings

---
+++ substrings.rb
@@ -0,0 +1,14 @@
+def substrings(string, dictionary)
+  words = string.downcase.split(" ")
+  result = Hash.new(0)
+
+  dictionary.each do |word|
+    words.each {|search_word| result[word] += 1 if search_word.include?(word)}
+  end
+  p result
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# {"down"=>1, "go"=>1, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3, "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}
